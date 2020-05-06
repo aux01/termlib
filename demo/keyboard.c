dbd5569ba16f3061bd46a8a83fe6d6d83bf7a758
Auxrelius I <aux01@aux.life>
2020-05-06 16:36:23 -0500

    Fix gcc warning in demo/keyboard.c
    Avoid sprintf'ing into yourself.

--- demo/keyboard.c
+++ demo/keyboard.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
 #include "../termbox.h"
 
 struct key {
@@ -521,8 +522,11 @@ void draw_keyboard()
 	if (inputmode & TB_INPUT_ALT)
 		sprintf(inputmode_str, "TB_INPUT_ALT");
 
-	if (inputmode & TB_INPUT_MOUSE)
-		sprintf(inputmode_str, "%s | TB_INPUT_MOUSE", inputmode_str);
+	if (inputmode & TB_INPUT_MOUSE) {
+                char temp[32];
+                strcpy(temp, inputmode_str);
+		sprintf(inputmode_str, "%s | TB_INPUT_MOUSE", temp);
+        }
 
 	printf_tb(3, 18, TB_WHITE, TB_DEFAULT, "Input mode: %s", inputmode_str);
 }
